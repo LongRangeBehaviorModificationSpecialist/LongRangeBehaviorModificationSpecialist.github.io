05d8b3d277bc06456e55b3a6f32131833491df98
LongRangeBehaviorModificationSpecialist <31972048+LongRangeBehaviorModificationSpecialist@users.noreply.github.com>
2023-10-20 15:09:16 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,58 +1,74 @@
-function date() {
+function date()
+{
     var today = new Date();
     document.getElementById("date").innerHTML = today.toDateString();
 }
-
-
-function clock() {
+    
+function clock()
+{
     var today = new Date();
     var hour = zeros(twelveHour(today.getHours()));
     var minutes = zeros(today.getMinutes());
     var seconds = zeros(today.getSeconds());
-    if(today.getHours() >= 12) {
-        seconds += " PM"
-    } else {
-        seconds += " AM"
+    if(today.getHours() >= 12)
+    {
+        seconds += " PM";
+    }
+    else
+    {
+        seconds += " AM";
     }
     hrs = today.getHours();
     if (hrs < 12)
+    {
         greet = "Good Morning  ";
+    }
     else if (hrs >= 12 && hrs <= 17)
+    {
         greet = "Good Afternoon ";
+    }
     else if (hrs >= 17 && hrs <= 24)
+    {
         greet = "Good Evening  ";
+    }
     // console.log(today.toLocaleTimeString());
     document.getElementById("greet").innerHTML = greet;
     document.getElementById("hour").innerHTML = hour;
     document.getElementById("min").innerHTML = minutes;
     document.getElementById("sec").innerHTML = seconds;
 }
-
-
-function twelveHour(hour) {
-    if (hour > 12) {
-        return hour -= 12 
-    } else if (hour === 0) {
+    
+function twelveHour(hour)
+{
+    if (hour > 12)
+    {
+        return hour -= 12;
+    }
+    else if (hour === 0)
+    {
         return hour = 12;
-    } else {
-        return hour
+    }
+    else
+    {
+        return hour;
     }
 }
-
-
-// adds zero infront of single digit number
-function zeros(num) {
-    if (num < 10) {
-        num = "0" + num
+    
+// adds zero in front of single digit number
+function zeros(num)
+{
+    if (num < 10)
+    {
+        num = "0" + num;
     };
     return num;
 }
-
-
-function dateTime() {
+    
+function dateTime()
+{
     date();
     clock();
     setTimeout(dateTime, 500);
 }
-
+    
 dateTime()
